8e7b1f750bd692550cbb4987c04a342e82482d2c
Jacob Marks <jamarks13@gmail.com>
2023-10-19 14:22:38 -0700

    flexibility for prediction fields

--- __init__.py
+++ __init__.py
@@ -93,6 +93,64 @@ def _get_target_view(ctx, target):
     return ctx.view
 
 
+def _handle_prediction_fields(ctx, inputs):
+    obj = types.Object()
+    obj.bool(
+        "store_word_preds",
+        label="Store word predictions?",
+        default=False,
+        view=types.SwitchView(space=4),
+    )
+    obj.bool(
+        "store_block_preds",
+        label="Store block predictions?",
+        default=True,
+        view=types.SwitchView(space=4),
+    )
+    inputs.define_property("prediction_field_types", obj)
+
+    pfts = ctx.params.get("prediction_field_types", {})
+    store_word_preds = pfts.get("store_word_preds", False)
+    store_block_preds = pfts.get("store_block_preds", True)
+
+    if not store_word_preds and not store_block_preds:
+        inputs.view(
+            "warning",
+            types.Warning(
+                label="Not storing any predictions",
+                description=(
+                    "You have chosen not to store any predictions. "
+                    "You must store at least one type of prediction to "
+                    "use the `OCR` operation. "
+                ),
+            ),
+        )
+
+    if store_word_preds:
+        inputs.str(
+            "word_predictions_field",
+            label="Word predictions field",
+            default="pt_word_predictions",
+            description="The field in which to store the word predictions",
+            required=True,
+        )
+
+    if store_block_preds:
+        inputs.str(
+            "block_predictions_field",
+            label="Block predictions field",
+            default="pt_block_predictions",
+            description="The field in which to store the block predictions",
+            required=True,
+        )
+
+
+def _get_prediction_fields(ctx):
+    word_preds_field = ctx.params.get("word_predictions_field", None)
+    block_preds_field = ctx.params.get("block_predictions_field", None)
+    return word_preds_field, block_preds_field
+
+
 class OCR(foo.Operator):
     @property
     def config(self):
@@ -123,12 +181,13 @@ class OCR(foo.Operator):
         form_view = types.View(
             label="OCR",
             description=(
-                "Run optical character recognition on your images to"
+                "Run optical character recognition on your images to "
                 "detect text with PyTesseract"
             ),
         )
         _execution_mode(ctx, inputs)
         _list_target_views(ctx, inputs)
+        _handle_prediction_fields(ctx, inputs)
         return types.Property(inputs, view=form_view)
 
     def execute(self, ctx):
@@ -137,14 +196,19 @@ class OCR(foo.Operator):
 
         view = _get_target_view(ctx, ctx.params["target"])
 
+        word_preds_field, block_preds_field = _get_prediction_fields(ctx)
+        # block_preds_field = "my_blocks"
+
         with add_sys_path(os.path.dirname(os.path.abspath(__file__))):
             # pylint: disable=no-name-in-module,import-error
             from ocr_engine import get_ocr_detections
 
             for sample in view.iter_samples(autosave=True):
                 word_dets, block_dets = get_ocr_detections(sample)
-                sample["pt_word_predictions"] = word_dets
-                sample["pt_block_predictions"] = block_dets
+                if word_preds_field:
+                    sample[word_preds_field] = word_dets
+                if block_preds_field:
+                    sample[block_preds_field] = block_dets
         dataset.add_dynamic_sample_fields()
         ctx.trigger("reload_dataset")
 
